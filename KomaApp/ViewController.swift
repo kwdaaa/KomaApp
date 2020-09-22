0359bf41df51054413dfd725717ef61783fcf3e0
Shu1-net <shuichi.ama@gmail.com>
2020-09-22 13:00:18 +0900

    Initial Commit

---
+++ KomaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KomaApp
+//
+//  Created by 天野修一 on 2020/09/22.
+//  Copyright © 2020 shuichiama.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
