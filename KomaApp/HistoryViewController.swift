5f52d950e160d85be532c5d9f2269995d5f37c7b
Shu1-net <shuichi.ama@gmail.com>
2020-09-25 00:09:27 +0900

    first commit

---
+++ KomaApp/HistoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryViewController.swift
+//  KomaApp
+//
+//  Created by 天野修一 on 2020/09/23.
+//  Copyright © 2020 shuichiama.com. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- KomaApp/ViewController.swift
+++ KomaApp/ViewController.swift
@@ -8,13 +8,137 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    //class変数
+    
+    var whiteImageArray = [UIImage]()
+    var blackImageArray: [UIImage] = [UIImage]()
+ 
+    var blackImageViewArray: [UIImageView] = [UIImageView]()
+    var whiteImageViewArray: [UIImageView] = [UIImageView]()
+    
+    var scrollCount: Int = 0
+    
+    
+    
+      //紐付けs
+    @IBOutlet var scrollView: UIScrollView!
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    
+    @IBAction func whiteBlackSegment(_ sender: Any) {
+        
+        
+    }
+    
+    @IBOutlet var textField: UITextField!
+    
+    
+    @IBOutlet var pickerView: UIPickerView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        //準備用code
+        //arrayにUIImage画像を格納する(自動的に増やせる)
+        while let imageB = UIImage(named: "fukidashiB\(blackImageArray.count)") {
+            blackImageArray.append(imageB)
+        }
+        while let imageW = UIImage(named: "fukidashiW\(whiteImageArray.count)") {
+            whiteImageArray.append(imageW)
+        }
+       
+        //上で格納したarrayから、ImageViewのarrayに画像を移す
+        for i in blackImageArray {
+            
+            let imageView = UIImageView()
+            imageView.image = UIImageView(blackImageArray[i])
+            
+            blackImageViewArray.append(UIImageView(image: ))
+        }
+        
+        for i in whiteImageArray {
+                whiteImageViewArray.append(UIImageView(image: i))
+        }
 
+        print("UIImageViewArrayだよ:",blackImageViewArray)
+        print("UIImageViewArrayだよ:",whiteImageViewArray)
+
+        
+//        setupScrollView()
+
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setupScrollView()
+    }
+    
+    
+    func setupScrollView() {
+        
+        //scrollViewの準備
+        scrollView = UIScrollView(frame: view.bounds)
+        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        scrollView.backgroundColor = .white
+        scrollView.contentSize = imageView.bounds.size
+        scrollView.delegate = self
+        scrollView.addSubview(imageView)
+        
+        scrollView.isPagingEnabled = true
+        scrollView.isScrollEnabled = true
+
+        view.addSubview(scrollView)
+        
+        
+        scrollViewContent()
+        
+        
+    }
+    
+    
+    func scrollViewContent() {
+        /*
+        let imageView = blackImageViewArray[scrollCount]
+        //変数の枠を設定
+        imageView.frame = CGRect(x: UIScreen.main.bounds.width * 0.0, y: 0.0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        imageView.contentMode = UIView.ContentMode.scaleAspectFit
+        scrollView.addSubview(imageView)
+            
+            scrollCount += 1
+        */
+        
+        let firstImageView = blackImageViewArray[0]
+        //変数の枠を設定
+        firstImageView.frame = CGRect(x: UIScreen.main.bounds.width * 0.0, y: 0.0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        firstImageView.contentMode = UIView.ContentMode.scaleAspectFit
+        
+        scrollView.addSubview(firstImageView)
+        
+        
+        
+        let secondImageView = blackImageViewArray[1]
+        //変数の枠を設定
+        secondImageView.frame = CGRect(x: UIScreen.main.bounds.width * 0.0, y: 0.0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        secondImageView.contentMode = UIView.ContentMode.scaleAspectFit
+        scrollView.addSubview(secondImageView)
+        
+        
+    }
+    
+    
+ 
+
+    override func didReceiveMemoryWarning() {
+           super.didReceiveMemoryWarning()
+       }
+    
+    
+  
+
+   
 
 }
 
